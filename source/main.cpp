738850e45d4c98b4c4735caad7beb7cb5890fa69
Gio <GiovanH@users.noreply.github.com>
2018-12-09 21:32:55 -0600

    Source

---
+++ source/main.cpp
@@ -0,0 +1,22 @@
+#include <switch.h>
+
+#include "SysModule.hpp"
+//#include "main.h"
+
+//static long frameLength = 3000000L;
+
+int main(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+    //initLogs();
+
+    // while (true)
+    // {
+        //inputPoller();
+    //     svcSleepThread(3000000L);
+    // }
+
+    //closeLogs();
+    return 0;
+}
